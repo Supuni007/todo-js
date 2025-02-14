cac8440cb8b342825866d6b9309c2a6af179a7e3
Supuni Rathnayake <supunirathnayake007@gmail.com>
2025-02-14 08:03:03 +0530

    Add button

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-const txtTask = document.getElementById("te=xtTask");
+const txtTask = document.getElementById("txtTask");
 const sub = document.getElementById("sub");
 const results = document.getElementById("results");
 
-sub.addEventListener("click", function() {
+sub.addEventListener("click", function(e) {
+    e.preventDefault()
     const listItem = document.createElement("li");
     const addItem = document.createElement("span");
     const deleteButton = document.createElement("button");
     const doneButton = document.createElement("button");
 
     addItem.classList.add("item");
+    addItem.innerText = txtTask.value;
     deleteButton.classList.add("delete");
+    deleteButton.innerText = "Del";
     doneButton.classList.add("done");
+    doneButton.innerText = "Done";
+
+    listItem.appendChild(addItem);
+    listItem.appendChild(doneButton);
+    listItem.appendChild(deleteButton);
+    results.appendChild(listItem);
 })
