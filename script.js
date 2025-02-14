a736a36201756ed66120a727f5defcd41d71fb54
Supuni Rathnayake <supunirathnayake007@gmail.com>
2025-02-14 07:24:34 +0530

    Create elements and add class to them

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const txtTask = document.getElementById("te=xtTask");
 const sub = document.getElementById("sub");
 const results = document.getElementById("results");
+
+sub.addEventListener("click", function() {
+    const listItem = document.createElement("li");
+    const addItem = document.createElement("span");
+    const deleteButton = document.createElement("button");
+    const doneButton = document.createElement("button");
+
+    addItem.classList.add("item");
+    deleteButton.classList.add("delete");
+    doneButton.classList.add("done");
+})
